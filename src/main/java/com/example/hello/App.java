10c814e2ea7504a6cddeadc3d73010b6f53293d3
friersd <ddfrierson@yahoo.com>
2025-06-17 22:02:43 +0000

    Testing git webhook with jenkins again

--- src/main/java/com/example/hello/App.java
+++ src/main/java/com/example/hello/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println( "Trying Webhook within Jenkins Demo!" );
+        System.out.println( "Trying Webhook within Jenkins Demo Test!" );
     }
 }
